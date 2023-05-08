5aacd51805e2ea7283b2eb056c939a4c7fd4bf11
Alan Gou <alanjgou@gmail.com>
2023-05-08 16:25:15 -0400

    Update caching strategy

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
+const cacheStrategies = require("./serviceWorker/cache");
+
 /** @type {import('next').NextConfig} */
 const withPWA = require("next-pwa")({
   dest: "public",
+  runtimeCaching: cacheStrategies,
 });
 
 module.exports = withPWA({
