bd4384d6d0382c51dd9cc0f69006a106af9dfcc2
Alan Gou <alanjgou@gmail.com>
2023-05-10 14:38:08 -0600

    Add spinner

--- src/components/DownloadAudioButton.tsx
+++ src/components/DownloadAudioButton.tsx
@@ -2,15 +2,19 @@ import {
   ArrowDownTrayIcon,
   //   CheckCircleIcon,
   CheckIcon,
+  XMarkIcon,
 } from "@heroicons/react/20/solid";
 import { useCallback, useEffect, useState } from "react";
 import { DAO_CDN_MP3_CACHE } from "../consts";
+import { Spinner } from "./Spinner";
+import clsx from "clsx";
 
 export function DownloadAudioButton({ audioUrl }: { audioUrl: string }) {
   const [isCached, setIsCached] = useState(false);
+  const [isDownloading, setIsDownloading] = useState(false);
 
   const checkCache = useCallback(async () => {
-    const isCached = await isAudioCached(audioUrl);
+    const isCached = await checkAudioCached(audioUrl);
     setIsCached(isCached);
   }, [audioUrl]);
 
@@ -18,24 +22,38 @@ export function DownloadAudioButton({ audioUrl }: { audioUrl: string }) {
     checkCache();
   }, [checkCache]);
 
-  useEffect(() => {
-    function handleMessage(event: MessageEvent) {
-      if (event.data === "audio-cached") {
-        checkCache();
-      }
-    }
+  // useEffect(() => {
+  //   function handleMessage(event: MessageEvent) {
+  //     if (event.data === "audio-cached") {
+  //       checkCache();
+  //     }
+  //   }
 
-    navigator.serviceWorker.addEventListener("message", handleMessage);
+  //   navigator.serviceWorker.addEventListener("message", handleMessage);
 
-    return () => {
-      navigator.serviceWorker.removeEventListener("message", handleMessage);
-    };
-  }, [checkCache]);
+  //   return () => {
+  //     navigator.serviceWorker.removeEventListener("message", handleMessage);
+  //   };
+  // }, [checkCache]);
 
   const onClick = async () => {
+    const isCached = await checkAudioCached(audioUrl);
+    if (isCached) {
+      setIsCached(true);
+      return;
+    }
+
+    setIsDownloading(true);
+    setTimeout(() => {
+      setIsDownloading(false);
+    }, 10000);
+    await fetch(audioUrl);
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    await checkCache();
+    setIsDownloading(false);
     // await fetch(audioUrl);
 
-    await checkCache();
+    // await checkCache();
     // Send a message to the service worker to cache the audio file
     // if (navigator.serviceWorker.controller) {
     //   navigator.serviceWorker.controller.postMessage({
@@ -49,11 +67,30 @@ export function DownloadAudioButton({ audioUrl }: { audioUrl: string }) {
     // await checkCache();
   };
 
+  const onClearDownloadClick = async () => {
+    const cache = await caches.open(DAO_CDN_MP3_CACHE);
+    await cache.delete(audioUrl);
+    await checkCache();
+  };
+
   if (isCached) {
     return (
-      <div>
-        <CheckIcon className="text-green-500 w-4 h-4" />
-      </div>
+      <button onClick={onClearDownloadClick} className="group">
+        <CheckIcon className="text-green-500 w-4 h-4 group-hover:hidden" />
+        <XMarkIcon className="text-red-500 w-4 h-4 hidden group-hover:block" />
+      </button>
+    );
+  }
+
+  if (isDownloading) {
+    return (
+      <button className="group">
+        <Spinner
+          className={clsx(
+            "w-5 h-5 mr-2 text-gray-200 dark:text-gray-600 fill-gray-800"
+          )}
+        />
+      </button>
     );
   }
 
@@ -64,7 +101,7 @@ export function DownloadAudioButton({ audioUrl }: { audioUrl: string }) {
   );
 }
 
-async function isAudioCached(audioUrl: string) {
+async function checkAudioCached(audioUrl: string) {
   if (!("caches" in window)) {
     return false;
   }
