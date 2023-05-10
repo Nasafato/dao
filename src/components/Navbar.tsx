3bd4e739009bcbfaaccea818d80056a059f0d8f3
Alan Gou <alanjgou@gmail.com>
2023-05-10 15:54:12 -0600

    Add dark mode

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,14 +4,7 @@ export function Navbar() {
   const { toggleTheme, theme } = useTheme();
   return (
     <nav className="px-8 lg:px-24 py-8">
-      <div className="m-auto max-w-xl items-center justify-between font-mono text-sm border-b border-gray-200">
-        <button
-          onClick={() => toggleTheme()}
-          className="hover:text-gray-600 dark:hover:text-gray-300"
-        >
-          {theme === "light" ? "Light" : "Dark"}
-        </button>
-      </div>
+      <div className="m-auto max-w-xl items-center justify-between font-mono text-sm border-b border-gray-200"></div>
     </nav>
   );
 }
