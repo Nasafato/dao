9236e8f6e9f37a6ca7cd4c075ca63e2ba9f9c334
Alan Gou <alanjgou@gmail.com>
2023-05-10 22:11:02 -0600

    Use more tailwind UI to create dark mode

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -17,7 +17,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
       <Component {...pageProps} />
-      <ReactQueryDevtools initialIsOpen={false} />
+      {/* <ReactQueryDevtools initialIsOpen={false} /> */}
     </QueryClientProvider>
   );
 }
