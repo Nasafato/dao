b57850c7877eb991b2425faafaed794ae995503e
apollo9981 <alanjgou@gmail.com>
2023-06-01 13:50:19 -0400

    DAO-57: Try to sync search params in dictionary page (#19)

--- src/app/dictionary/Dictionary.tsx
+++ src/app/dictionary/Dictionary.tsx
@@ -1,19 +1,12 @@
 "use client";
 
 import { MagnifyingGlassCircleIcon } from "@heroicons/react/20/solid";
+import { useQuery } from "@tanstack/react-query";
 import { useRouter, useSearchParams } from "next/navigation";
 import { FormEvent, useCallback, useEffect, useRef, useState } from "react";
-import { Spinner } from "../../components/shared/Spinner";
-import { DefinitionOutput } from "../../server/routers/_app";
-import {
-  buildPinyinWithTones,
-  replaceNumericalPinyin,
-  useQueryParam,
-} from "../../utils";
-import { api } from "../../utils/trpc";
 import { SingleCharDefinition } from "../../components/primary/SingleCharDefinition";
+import { Spinner } from "../../components/shared/Spinner";
 import { trpcClient } from "../../lib/trpcClient";
-import { useQuery } from "@tanstack/react-query";
 
 const LiStyle = "ring-1 ring-gray-300 rounded-md hover:bg-gray-100";
 const commonSearchTerms = ["药", "冰", "道", "名", "为", "圣"];
@@ -21,31 +14,32 @@ const commonSearchTerms = ["药", "冰", "道", "名", "为", "圣"];
 export function Dictionary() {
   const router = useRouter();
   const [searchTerm, setSearchTerm] = useState("");
+  const searchParams = useSearchParams();
+  const query = searchParams?.get("query");
+  useEffect(() => {
+    if (!query || query.length < 1) {
+      return;
+    }
 
-  //   const query = useQueryParam("query");
-  //   useEffect(() => {
-  //     setSearchTerm(query ?? "");
-  //     if (inputRef.current) {
-  //       inputRef.current.value = query ?? "";
-  //     }
-  //   }, [query]);
+    setSearchTerm(query);
+    if (inputRef.current) {
+      inputRef.current.value = query;
+    }
+  }, [query]);
 
   const updateSearchTerm = useCallback(
     (searchTerm: string) => {
-      //   if (searchTerm) {
-      //     // console.log("pushing", searchTerm);
-      //     router.push(`/dictionary?query=${encodeURIComponent(searchTerm)}`);
-      //   } else {
-      //     router.push("/dictionary");
-      //   }
+      if (searchTerm) {
+        router.push(`/dictionary?query=${encodeURIComponent(searchTerm)}`);
+      } else {
+        router.push("/dictionary");
+      }
       setSearchTerm(searchTerm);
       if (inputRef.current) {
         inputRef.current.value = searchTerm;
       }
     },
-    // See https://github.com/vercel/next.js/issues/18127.
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    []
+    [router]
   );
 
   const searchQuery = useQuery({
@@ -64,7 +58,6 @@ export function Dictionary() {
       return;
     }
     updateSearchTerm(value);
-    console.log(searchQuery);
   };
 
   const inputRef = useRef<HTMLInputElement>(null);
