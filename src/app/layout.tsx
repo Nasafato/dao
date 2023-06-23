8252d1906dea6196f6aa0b4e2d196b7894fe35fe
Alan Gou <alanjgou@gmail.com>
2023-06-23 08:11:53 -0400

    Update mode toggle to fix dark mode flash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import { Providers } from "./Providers";
 import { Popover } from "../components/primary/VersesPopover";
 import { twJoin } from "tailwind-merge";
 import { MainLayoutHorizontalPaddingStyle } from "../styles";
+import { modeToggleEffect } from "./modeToggleEffect";
 export default function RootLayout({
   children,
 }: {
@@ -15,6 +16,13 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-p-16">
+      <head>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `(${modeToggleEffect.toString()})()`,
+          }}
+        />
+      </head>
       <body className="bg-white text-gray-800 dark:bg-gray-950 dark:text-gray-100">
         <Providers>
           <Header />
@@ -33,12 +41,6 @@ export default function RootLayout({
         <div id="popover-portal"></div>
         <div id="command-palette-portal"></div>
       </body>
-      <Script
-        id="modeScript"
-        dangerouslySetInnerHTML={{
-          __html: modeScript,
-        }}
-      />
     </html>
   );
 }
@@ -47,38 +49,3 @@ export const metadata: Metadata = {
   title: "Home",
   description: "Welcome to the Daoedejing",
 };
-
-const modeScript = `
-  let darkModeMediaQuery = window.matchMedia('(prefers-color-scheme: dark)')
-
-  updateMode()
-  darkModeMediaQuery.addEventListener('change', updateModeWithoutTransitions)
-  window.addEventListener('storage', updateModeWithoutTransitions)
-
-  function updateMode() {
-    let isSystemDarkMode = darkModeMediaQuery.matches
-    let isDarkMode = window.localStorage.isDarkMode === 'true' || (!('isDarkMode' in window.localStorage) && isSystemDarkMode)
-
-    if (isDarkMode) {
-      document.documentElement.classList.add('dark')
-    } else {
-      document.documentElement.classList.remove('dark')
-    }
-
-    if (isDarkMode === isSystemDarkMode) {
-      delete window.localStorage.isDarkMode
-    }
-  }
-
-  function disableTransitionsTemporarily() {
-    document.documentElement.classList.add('[&_*]:!transition-none')
-    window.setTimeout(() => {
-      document.documentElement.classList.remove('[&_*]:!transition-none')
-    }, 0)
-  }
-
-  function updateModeWithoutTransitions() {
-    disableTransitionsTemporarily()
-    updateMode()
-  }
-`;
