c1cf443ebe761ef93d25fabf9e9e7b6b37af9ab4
apollo9981 <alanjgou@gmail.com>
2023-06-23 17:03:04 -0400

    DAO-78: Update the About page (#38)

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -4,25 +4,39 @@ export default function AboutPage() {
       <h1 className="font-bold">About</h1>
       <div className="space-y-2">
         <p>
-          I was born in Ann Arbor, Michigan, where my father was finishing up
-          his Ph.D. His birthplace was much farther away, across the Pacific
-          Ocean, in a little river town called Qijiang (綦江), now incorporated
-          as part of the Chongqing (重庆) Municipality. Back then, it was still
-          part of Sichuan (四川) province, which you may know for its famous
-          dishes like mapo tofu and dandan noodles.
+          The 道德经 (Dao De Jing) is a foundational Chinese text, compiled over
+          2000 years ago. It is short, terse, and often very difficult to parse.
+        </p>
+        <p>Read it at your own peril.</p>
+        <br />
+      </div>
+      <div>
+        <h2 className="font-bold">Why build this</h2>
+        <p>
+          I am trying to memorize the Dao, especially when I&apos;m on the go
+          and the Internet is spotty. The base text is viewable offline, and the
+          dictionary for the characters in the Dao is cached locally, as are the
+          audio recordings.
+        </p>
+      </div>
+      <div>
+        <h2 className="font-bold">Why memorize the Dao</h2>
+        <p>
+          Memories change you. What is your mind if not your brain plus the sum
+          of your past experiences? Those experiences leave grooves, and those
+          grooves turn into your thoughts and your actions as you contend with
+          the world.
         </p>
         <p>
-          My mother was born in Beijing, her father a journalist and college
-          graduate who had documented and wrote about his experiences in the
-          Korean War, her mother, a zookeeper, until she passed away when my
-          mother was twelve.
+          To memorize a text is to let it become part of you more fully than
+          just reading it once or twice. Most knowledge in the world is useless
+          within a week after learning it. You read enough books, blog posts,
+          Twitter threads, watch enough movies, and see that many things are
+          beautiful, much knowledge is intriguing, but how much of it lasts?
         </p>
         <p>
-          My father and mother met in the United States, part of a great wave of
-          student-scholars filtered by Deng&apos;s newly reinstated national
-          exams, sent forth to learn from a world that had long ago passed China
-          in every technological and economic sense. This was 改革开放, the
-          great era of Reformation and Opening.
+          So, I am memorizing the Dao. It is only 81 verses, totaling 5260
+          Chinese characters. A few months of work for a lifetime.
         </p>
       </div>
     </div>
