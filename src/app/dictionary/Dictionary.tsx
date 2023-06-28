f94fbdcfcdf1b6da9539837222da3166990e2169
Alan Gou <alanjgou@gmail.com>
2023-06-28 17:28:53 -0400

    More fallback

--- src/app/dictionary/Dictionary.tsx
+++ src/app/dictionary/Dictionary.tsx
@@ -7,7 +7,6 @@ import { FormEvent, useCallback, useEffect, useRef, useState } from "react";
 import { SingleCharDefinition } from "../../components/primary/SingleCharDefinition";
 import { Spinner } from "../../components/shared/Spinner";
 import { trpcClient } from "../../lib/trpcClient";
-import { button } from "../../styles";
 
 const LiStyle =
   "ring-1 ring-gray-300 rounded-md hover:bg-gray-100 dark:hover:bg-gray-800";
@@ -122,6 +121,51 @@ export function Dictionary() {
         ) : searchTerm.length > 0 ? (
           <div>Nothing found</div>
         ) : null}
+      </div>
+    </div>
+  );
+}
+
+export function Fallback() {
+  return (
+    <div className="py-2 max-w-sm mx-auto">
+      <div className="mb-4">
+        <h3 className="text-sm font-semibold mb-1">Common search terms</h3>
+        <ul className="flex gap-x-2">
+          {commonSearchTerms.map((term) => (
+            <li className={LiStyle} key={term}>
+              <button
+                className={`
+                  px-2 py-1
+                `}
+              >
+                {term}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+      <div className="relative w-full max-w-md">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <MagnifyingGlassCircleIcon
+            className="w-5 h-5 text-gray-400"
+            aria-hidden="true"
+          />
+        </div>
+        <form>
+          <input
+            type="text"
+            className={`
+              block w-full pl-10 pr-3 py-2 border sm:text-sm rounded-md leading-5 
+            bg-white dark:bg-inherit
+              border-gray-300 
+               placeholder-gray-500 
+              focus:outline-none focus:placeholder-gray-400 
+              focus:border-blue-300 focus:ring-blue-300 
+            `}
+            placeholder=""
+          />
+        </form>
       </div>
     </div>
   );
